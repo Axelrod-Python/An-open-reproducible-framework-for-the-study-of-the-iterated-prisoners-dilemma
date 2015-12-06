7f4a774542060d708dcf8d3a6dfb9bdf92378745
Vince Knight <vincent.knight@gmail.com>
2015-12-06 13:55:51 +0000

    Correcting the data.

--- scripts/number_of_strategies.py
+++ scripts/number_of_strategies.py
@@ -1,8 +1,42 @@
-import axelrod
 from sys import argv
+import csv
+
 
 try:
-    if type(len(axelrod.strategies)) is int:
-        print argv[1], len(axelrod.strategies), argv[2]
+    import axelrod
+
+    strategies = []
+
+    try:
+        if type(axelrod.strategies) is list:
+            strategies += axelrod.strategies
+    except (AttributeError, TypeError):
+        pass
+
+    try:
+        if type(axelrod.ordinary_strategies) is list:
+            strategies += axelrod.ordinary_strategies
+    except (AttributeError, TypeError):
+        pass
+
+    try:
+        if type(axelrod.basic_strategies) is list:
+            strategies += axelrod.basic_strategies
+    except (AttributeError, TypeError):
+        pass
+
+    try:
+        if type(axelrod.cheating_strategies) is list:
+            strategies += axelrod.cheating_strategies
+    except (AttributeError, TypeError):
+        pass
+
+    count = len(set(strategies))
+
+    f = open('data', 'a')
+    csvwrtr = csv.writer(f)
+    csvwrtr.writerow([argv[1], count, argv[2]])
+    f.close()
+
 except:
     pass
